89ae02bdaeb3ce8441a6f666511e64e1da4b78a0
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-04-25 00:50:41 +0900

    feat: Gun 스크립트에 멤버변수 추가

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 // 총 기능 구현 모듈
 public class Gun : MonoBehaviour
 {
+    // 총의 상태를 표현하는 enum
+    public enum State
+    {
+        Ready, // 발사 준비 상태
+        Empty, // 탄창이 빈 상태
+        Reloading // 재장전 상태
+    }
+
+    public State state { get; private set; } // 총의 상태값을 자동구현 프로퍼티로 접근 및 쓰기
+
+    public GunData gunData; // 총의 현재 데이터
+
+    public int ammoRemain; // 남아있는 모든 탄알 수
+    public int magAmmo; // 현재 탄창 안에 남아있는 탄알 수
+
+    private AudioSource gunAudioPlayer; // 총 효과음 재생
+
+    private float fireDistance = 50f; // 총의 사정거리 > 너무 멀리있는 오브젝트의 레이캐스팅 처리 제한 목적
+    private float lastFireTime; // 총을 마지막으로 발사한 시점
+
     [SerializeField]
     private Transform m_leftHandMount; // 총의 왼쪽 손잡이
     [SerializeField]
