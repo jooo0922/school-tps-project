6b9da48913021398e35b01607ca1a28415316450
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-04-25 01:39:06 +0900

    feat: 총 발사 및 발사 이펙트 로직 구현

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -62,12 +62,70 @@ public class Gun : MonoBehaviour
         lastFireTime = 0; // 총을 마지막으로 발사한 시점 초기화
     }
 
-
-    // Update is called once per frame
-    private void Update()
+    // 총 발사 트리거
+    public void Fire()
     {
-        
+        // 현재 상태가 발사 준비 상태인지,
+        // 마지막 총 발사 후, gunData.timeBetweenFire 만큼 시간이 지났는지 검사
+        if (state == State.Ready && Time.time >= lastFireTime + gunData.timeBetweenFire)
+        {
+            lastFireTime = Time.time; // 마지막 발사 시점을 현재 시점으로 초기화
+            Shot(); // 실제 발사 처리
+        }
     }
 
-    
+    // 실제 발사 처리
+    private void Shot()
+    {
+        RaycastHit hit; // 레이캐스트 충돌정보 저장하는 컨테이너 변수
+        Vector3 hitPosition = Vector3.zero; // 레이캐스팅 충돌 지점 좌표를 저장할 변수 초기화
+
+        // 레이캐스트 검사 실행 (레이캐스틴 시작 지점, 레이캐스팅 방향, 레이캐스팅 결과 저장 변수, 레이캐스팅 유효거리)
+        if (Physics.Raycast(firePosition.position, firePosition.forward, out hit, fireDistance))
+        {
+            // 레이가 어떤 물체와 교차했을 때의 처리
+            IDamageable target = hit.collider.GetComponent<IDamageable>(); // 교차한 오브젝트들 중에서 IDamageable 을 상속받는 컴포넌트를 갖고 있는 오브젝트 가져오기
+            if (target != null)
+            {
+                // 충돌한 오브젝트들 중에서 IDamageable 인터페이스를 상속받는 컴포넌트가 존재하는 오브젝트가 존재할 때의 처리
+                // IDamageable 인터페이스의 OnDamage() 메서드 실행 (깎을 데미지, 충돌 지점 좌표, 충돌 표면 방향벡터 전달)
+                target.OnDamage(gunData.damage, hit.point, hit.normal);
+            }
+
+            hitPosition = hit.point; // 레이캐스팅 충돌 지점 좌표 저장
+        }
+        else
+        {
+            // 레이가 교체한 물체가 없을 때의 처리
+            // 충돌 지점 좌표를 현재 총구위치를 기준으로 총구 방향벡터를 따라 최대 사정거리까지 날아간 지점의 좌표로 계산
+            hitPosition = firePosition.position + firePosition.forward * fireDistance;
+        }
+
+        StartCoroutine(ShotEffect(hitPosition)); // 발사 이펙트 처리 코루틴 메서드 실행
+
+        magAmmo--; // 현재 탄창 탄알 수 감소시킴
+        if (magAmmo <= 0)
+        {
+            state = State.Empty; // 탄창에 남은 탄알 수가 0보다 작다? > 탄창이 빈 상태로 상태 변경
+        }
+    }
+
+    // 발사 이펙트 처리하는 코루틴 메서드
+    private IEnumerator ShotEffect(Vector3 hitPoisition)
+    {
+        gunAudioPlayer.PlayOneShot(gunData.fireClip); // 총 발사 오디오 재생 (총 연속 발사 시, 기존 효과음과 자연스럽게 중첩 처리 -> .PlayOnmeShot())
+        yield return new WaitForSeconds(0.03f);
+    }
+
+    // 재장전 트리거
+    public bool Reload()
+    {
+        return true;
+    }
+
+    // 실제 재장전 처리 코루틴 메서드
+    private IEnumerator ReloadRoutine()
+    {
+        yield return new WaitForSeconds(gunData.reloadTime);
+    }
 }
