083d6d8030efd09ae61508fa3bd070eaff041f62
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-05-09 22:05:54 +0900

    feat: Mimic 스크립터블 오브젝트 데이터 컨테이너 생성

---
+++ Assets/Scripts/Monsters/Mimic/MimicData.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// Mimic 생성 시 사용할 스크립터블 오브젝트 데이터
+[CreateAssetMenu(menuName = "Scriptable/MimicData", fileName = "Mimic Data")]
+public class MimicData : ScriptableObject
+{
+    [Header("Mimic Stats")]
+    public float health = 100f;
+    public float damage = 5f;
+    public float speed = 3f;
+    public float height = 0.5f;
+
+    [Header("Legs")]
+    [Tooltip("Leg placement radius offset")]
+    public float newLegRadius = 3f;
+    [Tooltip("Minimum leg distance from center of mimic")]
+    public float minLegDistance = 4.5f;
+
+    [Header("Collider")]
+    [Tooltip("Radius of Sphere Collider applied to mimic")]
+    public float radius = 0.8f;
+
+    [Header("Color")]
+    public Color mimicColor = new Color(0.019f, 0f, 0.086f);
+}
