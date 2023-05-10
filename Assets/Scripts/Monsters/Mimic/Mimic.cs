1e4b2f2d072d1ee29554865a6aff094d800aefa6
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-05-11 01:55:15 +0900

    style: 공백 줄 제거

--- Assets/Scripts/Monsters/Mimic/Mimic.cs
+++ Assets/Scripts/Monsters/Mimic/Mimic.cs
@@ -90,7 +90,6 @@ namespace MimicSpace
             velocity = new Vector3(randV.x, 0, randV.y);
             minimumAnchoredParts = minimumAnchoredLegs * partsPerLeg;
             maxLegDistance = newLegRadius * 2.1f;
-
         }
 
         IEnumerator NewLegCooldown()
