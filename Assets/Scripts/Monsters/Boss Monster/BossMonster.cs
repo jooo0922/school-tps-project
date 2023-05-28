1dfe9803a26c43837eefa4e04f5007d5d5f8102a
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-05-28 13:57:40 +0900

    feat: Boss Monster > 오디오 소스 컴포넌트 추가 및 피격 오디오 재생 구현

--- Assets/Scripts/Monsters/Boss Monster/BossMonster.cs
+++ Assets/Scripts/Monsters/Boss Monster/BossMonster.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BossMonster : LivingEntity
 {
+    [Header("Effects")]
+    public AudioClip hitSound; // 피격 시 재생할 오디오
+
+    private AudioSource bossMonsterAudio; // 오디오 소스 컴포넌트
 
     // 공격받았을 때의 처리 override
     public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
@@ -11,9 +15,11 @@ public class BossMonster : LivingEntity
         if (!dead)
         {
             // 공격받았을 때의 처리
+            bossMonsterAudio.PlayOneShot(hitSound); // 피격 오디오 재생
         }
 
         base.OnDamage(damage, hitPoint, hitNormal);
+        // TODO: UI 업데이트 처리
     }
 
     public override void Die()
@@ -22,9 +28,9 @@ public class BossMonster : LivingEntity
     }
 
     // Start is called before the first frame update
-    private void Start()
+    private void Awake()
     {
-        
+        bossMonsterAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
