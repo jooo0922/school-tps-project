aed514427d459286b43f84655381416c0fd88415
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-06-01 00:48:32 +0900

    fix: 불필요한 GPU 인스턴싱 로직 제거

--- Assets/Scripts/Monsters/Blob/BlobSpawner.cs
+++ Assets/Scripts/Monsters/Blob/BlobSpawner.cs
@@ -21,36 +21,22 @@ public class BlobSpawner : MonoBehaviour
     public float minScale = 0.1f;
     public float maxScale = 2.5f;
 
-    private Mesh blobMesh; // Blob 메시
-    private Material blobMaterial; // Blob 머티리얼
-
-    private Matrix4x4[] matrices; // 인스턴스 변환 행렬 배열
-
     // GPU 인스턴싱을 위한 멤버변수 초기화 및 각 인스턴스 변환 계산
     private void Setup()
     {
-        blobMesh = blobPrefab.GetComponentInChildren<MeshFilter>().sharedMesh; // Blob 메쉬 가져오기
-        blobMaterial = blobPrefab.GetComponentInChildren<Renderer>().sharedMaterial; // Blob 머티리얼 가져오기
-
-        matrices = new Matrix4x4[instanceCount]; // 변환행렬 배열 사이즈를 인스턴스 개수만큼 초기화
-
         for (int i = 0; i < instanceCount; i++)
         {
             Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
             Quaternion rotation = Quaternion.identity;
             Vector3 scale = Vector3.one * Random.Range(minScale, maxScale);
 
-            matrices[i] = Matrix4x4.TRS(position, rotation, scale);
+            GameObject blob = Instantiate(blobPrefab, position, rotation);
+            blob.transform.localScale = scale;
         }
     }
 
     private void Start()
     {
         Setup();
-    }
-
-    private void Update()
-    {
-        Graphics.DrawMeshInstanced(blobMesh, 0, blobMaterial, matrices); // 매 프레임마다 GPU 인스턴스 렌더링
     }
 }
