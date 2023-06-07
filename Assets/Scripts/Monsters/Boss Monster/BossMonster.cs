2340d4182bda4a7c3b10ed5395faba7ce4b7d9dc
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-06-08 01:51:34 +0900

    feat: 보스몬스터 체력 UI 활성화, 비활성화, 업데이트 메서드 호출

--- Assets/Scripts/Monsters/Boss Monster/BossMonster.cs
+++ Assets/Scripts/Monsters/Boss Monster/BossMonster.cs
@@ -9,22 +9,31 @@ public class BossMonster : LivingEntity
 
     private AudioSource bossMonsterAudio; // 오디오 소스 컴포넌트
 
+    private bool isFirstAttack = true; // 첫 공격 여부 
+
     // 공격받았을 때의 처리 override
     public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
     {
         if (!dead)
         {
+            if (isFirstAttack)
+            {
+                UIManager.instance.ShowBossHealth(); // 첫 공격 시 보스 체력 바를 보여줌
+                isFirstAttack = false;
+            }
+
             // 공격받았을 때의 처리
             bossMonsterAudio.PlayOneShot(hitSound); // 피격 오디오 재생
         }
 
         base.OnDamage(damage, hitPoint, hitNormal);
-        // TODO: UI 업데이트 처리
+        UIManager.instance.UpdateBossHealth(health, startingHealth); // 보스몬스터 체력 UI 업데이트
     }
 
     public override void Die()
     {
         base.Die();
+        UIManager.instance.HideBossHealth(); // 보스몬스터 사망 시, 체력 UI 숨기기 
     }
 
     // Start is called before the first frame update
