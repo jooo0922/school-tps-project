ef87aee35df572858818711ca47fe57d6ea5c2c0
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-06-08 01:05:29 +0900

    feat: 무기 장착 및 변경 시, 총기 버튼 활성화 UI 업데이트 호출

--- Assets/Scripts/PlayerCharacter/PlayerGunManager.cs
+++ Assets/Scripts/PlayerCharacter/PlayerGunManager.cs
@@ -104,10 +104,12 @@ public class PlayerGunManager : MonoBehaviour
             if (isMounted)
             {
                 UIManager.instance.UpdateAmmoText(currentGun.magAmmo, currentGun.magCapacity); // 무기 착용 시, 현재 무기 탄창 UI 업데이트
+                UIManager.instance.UpdateGunButtonBox(currentGun.gunName); // 총기 버튼 활성화 UI 업데이트
             }
             else
             {
                 UIManager.instance.UpdateAmmoText(0, 0); // 무기 미착용 시, 탄창 UI 초기화
+                UIManager.instance.UpdateGunButtonBox(null); // 총기 버튼 활성화 UI 초기화
             }
         }
     }
@@ -122,6 +124,7 @@ public class PlayerGunManager : MonoBehaviour
             playerAudioSource.PlayOneShot(gunSwapClip); // 무기 교체 오디오 재생
 
             UIManager.instance.UpdateAmmoText(currentGun.magAmmo, currentGun.magCapacity); // 변경된 무기의 탄창 UI 업데이트
+            UIManager.instance.UpdateGunButtonBox(currentGun.gunName); // 총기 버튼 활성화 UI 업데이트
         }
     }
 
