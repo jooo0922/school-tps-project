3c6423c94f578e69539d810fa4a70d7e65a9d424
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-06-28 01:47:27 +0900

    feat: 씬 간 데이터 전송을 위한 스크립터블 오브젝트 데이터 생성

---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 씬 간 데이터 전송 목적으로 만든 스크립터블 오브젝트
+[CreateAssetMenu(menuName = "Scriptable/CharacterData", fileName = "Character Data")]
+public class CharacterData : ScriptableObject
+{
+    [SerializeField]
+    private string mSelectedCharacter; // Lobby 씬에서 선택된 캐릭터 이름
+
+    // selectedCharacter 데이터 읽기 / 쓰기를 제어하는 프로퍼티
+    public string selectedCharacter
+    {
+        get { return mSelectedCharacter; }
+        set { mSelectedCharacter = value; }
+    }
+}
