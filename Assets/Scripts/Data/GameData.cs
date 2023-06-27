7d08178f33bfe2a62ea7be070f1b789b34146d84
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-06-28 01:56:35 +0900

    rename: CharacterData > GameData 로 이름 변경

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 씬 간 데이터 전송 목적으로 만든 스크립터블 오브젝트
-[CreateAssetMenu(menuName = "Scriptable/CharacterData", fileName = "Character Data")]
-public class CharacterData : ScriptableObject
+[CreateAssetMenu(menuName = "Scriptable/GameData", fileName = "Game Data")]
+public class GameData : ScriptableObject
 {
     [SerializeField]
     private string mSelectedCharacter; // Lobby 씬에서 선택된 캐릭터 이름
