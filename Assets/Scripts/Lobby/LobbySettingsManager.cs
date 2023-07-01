2b3c2da0eebf447d2ee081046a73bafc66098921
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2023-07-01 19:14:42 +0900

    feat: 해상도 옵션 선택 기능 구현

--- Assets/Scripts/Lobby/LobbySettingsManager.cs
+++ Assets/Scripts/Lobby/LobbySettingsManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro; // TextMeshPro (TMP) 관련 코드
 
 public class LobbySettingsManager : MonoBehaviour
 {
     [Header("Audio")]
     public AudioMixer soundEffectVolume; // 효과음 오디오 믹서 에셋
     public AudioMixer backgroundVolume; // 배경음 오디오 믹서 에셋
+
+    [Header("Graphics")]
+    public TMP_Dropdown resolutionOptionDropdown; // 해상도 옵션 드롭다운
 
     // 효과음 볼륨 조절 메서드
     public void SetSoundEffectVolume(float volume)
@@ -21,10 +25,23 @@ public class LobbySettingsManager : MonoBehaviour
         backgroundVolume.SetFloat("vol", volume);
     }
 
-    // TODO: 그래픽 설정 제어 (해상도, 퀄리티)
+    // 그래픽 퀄리티 설정 메서드
     public void SetGraphicsQuality(int qualityIndex)
     {
-        QualitySettings.SetQualityLevel(qualityIndex); // 그래픽 퀄리티 설정
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    // 해상도 설정 메서드
+    public void SetResolution(int resolutionIndex)
+    {
+        string selectedResolution = resolutionOptionDropdown.options[resolutionIndex].text;
+
+        // 선택된 해상도 문자열 파싱
+        string[] resolutionParts = selectedResolution.Split("*");
+        int width = int.Parse(resolutionParts[0].Trim()); // 참고로 Trim() 은 문자열 공백 제거 
+        int height = int.Parse(resolutionParts[1].Trim());
+
+        Screen.SetResolution(width, height, true); // 해상도 설정
     }
 }
 
